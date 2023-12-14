45d2a0e8f647458a5a3a78a0c087f517762af57c
AshlieAlvarez <alvarezashlie1@gmail.com>
2023-12-14 14:15:31 -0500

    Implemented Unit Testing for RewardsValue, Tested & Working

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -18,13 +18,47 @@ public class RewardValueTests {
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
+/*  ARRANGE(set up conditions, i.e test data)
+    ACT (perform action, what is being tested)
+    ASSERT (validate expected outcome by assertion)
+ */
+
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        //Testing the conversion of cash value to miles
+
+        //100 dollar test condition
+        double cashValue = 100;
+
+        // Creating a RewardsValue Object with the cashValue
+        RewardValue rewardValue = new RewardValue(cashValue);
+
+        //Defining what our expected miles are by dividing the cash value by the conversion rate
+        int expectedMiles = (int) Math.round(cashValue/RewardValue.MILES_TO_CASH_CONVERSION_RATE);
+
+        //Validating expected outcome by checking if the miles value from RewardValue is the same as our expected miles.
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
+
+        //If they are the same, test passes but if not, it will fail.
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        //Testing the conversion of miles to cash value
+
+        //10,000 miles is the test condition
+        int milesValue = 10000;
+
+        //Creating a RewardsValue Object with the milesValue
+        RewardValue rewardValue = new RewardValue(milesValue);
+
+        //Defining what our expected cash is by multiplying the miles by the conversion rate
+        double expectedCash = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+
+        //Validating expected outcome by checking if the cash value from RewardValue is the same as our expected cash.
+        assertEquals(expectedCash, rewardValue.getCashValue());
+
+        //If they are the same, test passes but if not, it will fail.
+
     }
 }
